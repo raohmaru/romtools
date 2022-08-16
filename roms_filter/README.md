18146d1efc9e4f3e43f3800002139797df5e0db7
Raohmaru <namoracon@gmail.com>
2022-08-16 21:03:35 +0200

    Use extended parameters, add --nobios

--- roms_filter/README.md
+++ roms_filter/README.md
@@ -1,5 +1,5 @@
 # ROMs Filter
-Generates a list of filtered ROMs given a folder with zipped ROMs. Works best with [No-Intro ROMs](https://no-intro.org/).
+Generates an opinionated list of filtered ROMs given a folder with zipped ROMs. Works best with [No-Intro ROMs](https://no-intro.org/).
 
 The filter selects one ROM from a group of ROMs with the same name using the following criteria:
 - Country preference: USA, World, Europe, other countries.
@@ -9,10 +9,14 @@ The filter selects one ROM from a group of ROMs with the same name using the fol
 ## Usage
 `ruby roms_filter.rb -t [targetdir] [-o [output.file] -a -s [attr1[,attrN]] -np`
 
-### Arguments
-- `-t`  Target dir with the zipped ROMs.
-- `-o`  Output file where to write the filtered ROMs list.
-- `-d`  Dry run/Analyze mode. Prints output in the terminal.
-- `-s`  Skip ROMs that matches the comma-separated list of attributes.
-- `-np` Skip ROMs with the attributes "Beta", "Proto", "Sample" or "Demo".
-- `-h`  Display this help.
+### Parameters
+
+| Parameter | Description |
+|-----------|-------------|
+| `-t`, `--target` | Target directory with the zipped ROMs. |
+| `-o`, `--output` | Output file where to write the filtered ROMs list. If omitted the file _selection.txt will be used. |
+| `-d`, `--dryrun` | Dry run/Analyze mode. Prints output in the terminal. |
+| `-s`, `--skip` | Skip ROMs that matches the comma-separated list of attributes. |
+| `-np`, `--noproto` | Skip ROMs with the attributes "Beta", "Proto", "Sample" or "Demo". |
+| `-np`, `--nobios` | Skip BIOS roms. |
+| `-h`, `--help` | Display this help. |
--- roms_filter/roms_filter.rb
+++ roms_filter/roms_filter.rb
@@ -5,6 +5,7 @@ working_dir = '.'
 output = '_selection.txt'
 $analyze = false
 $skip_attrs = false
+$bios = true
 # Variables
 rom_count = 0
 tmp_roms = []
@@ -22,7 +23,7 @@ RX_NAME = /(^[^(]+)/
 HELP = <<eof
 ROMs filter 1.0
 ---------------
-Generates a list of filtered ROMs given a folder with zipped ROMs. Works best with No-Intro ROMs.
+Generates an opinionated list of filtered ROMs given a folder with zipped ROMs. Works best with No-Intro ROMs.
 The filter selects one ROM from a group of ROMs with the same name using the following criteria:
 - Country preference: USA, World, Europe, other countries
 - Highest version/revision number
@@ -32,30 +33,36 @@ Usage:
     ruby roms_filter.rb -t [targetdir] [-o [output.file] -d -s [attr1[,attrN]]]
 
 Arguments:
-    -t  Target dir with the zipped ROMs.
-    -o  Output file were write the filtered ROMs.
-    -d  Dry run/Analyze mode. Prints output in the terminal.
-    -s  Skip ROMs that matches the comma-separated list of attributes. Case insensitive.
-    -np Skip ROMs with the attributes "Beta", "Proto", "Sample" or "Demo".
-    -h  Display this help.
+    -t, --target    Target dir with the zipped ROMs.
+    -o, --output    Output file where to write the filtered ROMs list. If omitted the file _selection.txt will be used.
+    -d, --dryrun    Dry run/Analyze mode. Prints output in the terminal.
+    -s, --skip      Skip ROMs that matches the comma-separated list of attributes. Case insensitive.
+    -np, --noproto  Skip ROMs with the attributes "Beta", "Proto", "Sample" or "Demo".
+    -nb, --nobios   Skip BIOS roms.
+    -h, --help      Display this help.
 eof
 
 unless ARGV.empty?
   ARGV.each_with_index { |item, i|
-    if item == "-t"
+    if item == "-t" || item == "--target"
       working_dir = ARGV[i+1]
-    elsif item == "-o"
+    elsif item == "-o" || item == "--output"
       output = ARGV[i+1]
-    elsif item == "-d"
+    elsif item == "-d" || item == "--dryrun"
       $analyze = true
-    elsif item == "-s"
-      $skip_attrs = ARGV[i+1].split(',')
-    elsif item == "-np"
+    elsif item == "-s" || item == "--skip"
+      if !$skip_attrs
+        $skip_attrs = []
+      end
+      $skip_attrs += ARGV[i+1].split(',')
+    elsif item == "-np" || item == "--noproto"
       if !$skip_attrs
         $skip_attrs = []
       end
       $skip_attrs.push "Beta", "Proto", "Sample", "Demo"
-    elsif item == "-h"
+    elsif item == "-nb" || item == "--nobios"
+      $bios = false
+    elsif item == "-h" || item == "--help"
       puts HELP
       exit
     end
@@ -74,7 +81,7 @@ def filter_roms(roms, roms_attrs)
   points = Array.new(roms.length, 0)
   revs = {}
   chosen = 0
-  roms.each_with_index { |rom, i|
+  roms.each_with_index { |rom, i|  
     attrs = rom.scan(RX_ATTRS).flatten
     if attrs.length == 0
       puts "Invalid ROM (missing region): " + rom
@@ -113,6 +120,10 @@ def filter_roms(roms, roms_attrs)
         }
       }
       points[i] = -1 if skip
+    end
+    
+    if !$bios && rom.include?("[BIOS]")
+      points[i] = -1
     end
     
     chosen = i if points[i] >= points[chosen]
