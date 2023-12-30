e8d0817db678d674fbf89ea286b863946c8a93c7
Raohmaru <namoracon@gmail.com>
2023-12-30 16:51:10 +0100

    Improve logic to choose higher version of ROMs.
    Removed GameCube filter (use `-pa GameCube`).
    Renamed `fa, --forceattr` -> `-pa, --preferattr`.

--- roms_filter/README.md
+++ roms_filter/README.md
@@ -5,25 +5,26 @@ Generates an opinionated list of filtered ROMs given a folder with zipped ROMs. 
 The filter selects one ROM from a group of ROMs with the same name using the following criteria:
 - Country preference: USA, World, Europe (and other countries unless `--exclude` is present).
 - Highest version/revision number.
-- GameCube reedition of NES games.
+
+If the input is a file with a carriage return-separated list of ROMs, it works better if the list is sorted alphabetically.
 
 ## Usage
 `ruby roms_filter.rb -i dirOrFile [-o output.file -s attr1[,attrN] -c country1[,countryN] -e -np -nu -nm -nb -d]`
 
 ### Arguments
 
-| Argument             | Description |
-|:---------------------|:------------|
-| `-i`, `--input`      | Input dir with the zipped ROMs, or a file with a carriage return-separated list of ROMs. |
-| `-o`, `--output`     | Output file where to write the filtered ROMs list. If omitted the file _rom-selection.txt will be used. |
-| `-c`, `--countries`  | Country preference: a comma-separated list of countries, from more relevant to less. Default is `USA,World,Europe` |
-| `-e`, `--exclude`    | Countries that are not in the list of preferred countries will be skipped. |
-| `-d`, `--dryrun`     | Dry run/Analyze mode. Prints output in the terminal. |
-| `-sa`, `--skipattr`  | Skip ROMs that matches the comma-separated list of attributes. Case insensitive. |
-| `-sn`, `--skipname`  | Skip ROMs which name matches a word of the comma-separated list. Case insensitive. |
-| `-fa`, `--forceattr` | Force include ROMs that matches any of the comma-separated list of attributes. Case insensitive. |
-| `-np`, `--noproto`   | Skip ROMs with the attributes Beta, Proto, Sample, Demo or Program. |
-| `-nu`, `--nounl`     | Skip ROMs with the attributes Homebrew, Unl, Aftermarket, Pirate or Unknown. |
-| `-nm`, `--nomini`    | Skip mini console ROMs and virtual console ROMs. |
-| `-nb`, `--nobios`    | Skip BIOS ROMs. |
-| `-h`, `--help`       | Display the help. |
+| Argument              | Description |
+|:----------------------|:------------|
+| `-i`, `--input`       | Input dir with the zipped ROMs, or a file with a carriage return-separated list of ROMs. |
+| `-o`, `--output`      | Output file where to write the filtered ROMs list. If omitted the file _rom-selection.txt will be used. |
+| `-c`, `--countries`   | Country preference: a comma-separated list of countries, from more relevant to less. Default is `USA,World,Europe` |
+| `-e`, `--exclude`     | Countries that are not in the list of preferred countries will be skipped. |
+| `-d`, `--dryrun`      | Dry run/Analyze mode. Prints output in the terminal. |
+| `-sa`, `--skipattr`   | Skip ROMs that matches the comma-separated list of attributes. Case insensitive. |
+| `-sn`, `--skipname`   | Skip ROMs which name matches a word of the comma-separated list. Case insensitive. |
+| `-pa`, `--preferattr` | Includes ROMs that matches any of the comma-separated list of attributes. Case insensitive. |
+| `-np`, `--noproto`    | Skip ROMs with the attributes Beta, Proto, Sample, Demo or Program. |
+| `-nu`, `--nounl`      | Skip ROMs with the attributes Homebrew, Unl, Aftermarket, Pirate or Unknown. |
+| `-nm`, `--nomini`     | Skip mini console ROMs and virtual console ROMs. |
+| `-nb`, `--nobios`     | Skip BIOS ROMs. |
+| `-h`, `--help`        | Display the help. |
--- roms_filter/roms_filter.rb
+++ roms_filter/roms_filter.rb
@@ -7,7 +7,7 @@ $countries = ["USA", "World", "Europe"]
 $analyze = false
 $skip_names = false
 $skip_attrs = false
-$force_attrs = false
+$prefer_attrs = false
 $bios = true
 $exclude = false
 # Variables
@@ -26,25 +26,24 @@ Generates an opinionated list of filtered ROMs given a folder with zipped ROMs. 
 The filter selects one ROM from a group of ROMs with the same name using the following criteria:
 - Country preference: USA, World, Europe (and other countries unless `--exclude` is present).
 - Highest version/revision number.
-- GameCube reedition of NES games.
 
 Usage:
     ruby roms_filter.rb -i dirOrFile [-o output.file -sa attr1[,attrN] -c country1[,countryN] -e -np -nu -nm -nb -d]
 
 Arguments:
-    -i, --input      Target dir with the zipped ROMs, or a file with a carriage return-separated list of ROMs.
-    -o, --output     Output file where to write the filtered ROMs list. If omitted the file _rom-selection.txt will be used.
-    -c, --countries  Country preference: a comma-separated list of countries, from more relevant to less. Default is USA,World,Europe
-    -e, --exclude    Countries that are not in the list of preferred countries will be skipped.
-    -d, --dryrun     Dry run/Analyze mode. Prints output in the terminal.
-    -sn, --skipname  Skip ROMs which name matches a word of the comma-separated list. Case insensitive.
-    -sa, --skipattr  Skip ROMs that matches the comma-separated list of attributes. Case insensitive.
-    -fa, --forceattr Force include ROMs that matches any of the comma-separated list of attributes. Case insensitive.
-    -np, --noproto   Skip ROMs with the attributes Beta, Proto, Sample, Demo or Program.
-    -nu, --nounl     Skip ROMs with the attributes Homebrew, Unl, Aftermarket, Pirate or Unknown.
-    -nm, --nomini    Skip mini console ROMs and virtual console ROMs.
-    -nb, --nobios    Skip BIOS ROMs.
-    -h, --help       Display this help.
+    -i, --input       Target dir with the zipped ROMs, or a file with a carriage return-separated list of ROMs.
+    -o, --output      Output file where to write the filtered ROMs list. If omitted the file _rom-selection.txt will be used.
+    -c, --countries   Country preference: a comma-separated list of countries, from more relevant to less. Default is USA,World,Europe
+    -e, --exclude     Countries that are not in the list of preferred countries will be skipped.
+    -d, --dryrun      Dry run/Analyze mode. Prints output in the terminal.
+    -sn, --skipname   Skip ROMs which name matches a word of the comma-separated list. Case insensitive.
+    -sa, --skipattr   Skip ROMs that matches the comma-separated list of attributes. Case insensitive.
+    -pa, --preferattr Includes ROMs that matches any of the comma-separated list of attributes. Case insensitive.
+    -np, --noproto    Skip ROMs with the attributes Beta, Proto, Sample, Demo or Program.
+    -nu, --nounl      Skip ROMs with the attributes Homebrew, Unl, Aftermarket, Pirate or Unknown.
+    -nm, --nomini     Skip mini console ROMs and virtual console ROMs.
+    -nb, --nobios     Skip BIOS ROMs.
+    -h, --help        Display this help.
 eof
 
 unless ARGV.empty?
@@ -60,12 +59,13 @@ unless ARGV.empty?
     elsif item == "-sn" || item == "--skipname"
       $skip_names = [] if !$skip_names
       $skip_names += ARGV[i+1].split(',')
+      print $skip_names, "\n"
     elsif item == "-sa" || item == "--skipattr"
       $skip_attrs = [] if !$skip_attrs
       $skip_attrs += ARGV[i+1].split(',')
-    elsif item == "-fa" || item == "--forceattr"
-      $force_attrs = [] if !$force_attrs
-      $force_attrs += ARGV[i+1].split(',')
+    elsif item == "-pa" || item == "--preferattr"
+      $prefer_attrs = [] if !$prefer_attrs
+      $prefer_attrs += ARGV[i+1].split(',')
     elsif item == "-np" || item == "--noproto"
       $skip_attrs = [] if !$skip_attrs
       $skip_attrs.push "Beta", "Proto", "Sample", "Demo", "Program", "Debug"
@@ -91,7 +91,7 @@ end
 
 def filter_roms(roms)
   score = Array.new(roms.length, 0)
-  revs = {}
+  revs = []
   chosen = 0
   roms.each_with_index { |rom, i|
     attrs = rom.scan(RX_ATTRS).flatten
@@ -112,23 +112,11 @@ def filter_roms(roms)
       end
     }
     score[i] += cp
-    # Re-edition of some NES games for the GameCube
-    if rom.include?("GameCube Edition")
-      score[i] += 1
-    end
-    # Prefer latest versions
-    if rom =~ RX_HAS_VERSION_NUMBER
-      if !revs[countries[0]]
-        revs[countries[0]] = 0
-      end
-      revs[countries[0]] += 1
-      score[i] += 0.1 * revs[countries[0]]
-    end
     
-    if $force_attrs
+    if $prefer_attrs
       attrs.each { |a|
         a = a.downcase
-        $force_attrs.each { |fa|
+        $prefer_attrs.each { |fa|
           if a.include? fa.downcase
             score[i] += 0.1
           end
@@ -163,9 +151,27 @@ def filter_roms(roms)
     
     if !$bios && rom.include?("[BIOS]")
       score[i] = -1
-    end
+    end    
     
-    chosen = i if score[i] >= score[chosen]
+    # Add revisions which to apply a score later
+    if score[i] > -1 && rom =~ RX_HAS_VERSION_NUMBER
+      revs.push([i, rom[RX_HAS_VERSION_NUMBER]])
+    end
+  }
+  
+  # Prefer latest versions
+  if revs.length
+    revs.sort!{ |a, b|
+      a[1] <=> b[1]
+    }
+    revs.each_with_index{ |x, i|
+      score[x[0]] += 0.1 * (i + 1)
+    }
+  end
+  
+  # Choose the rom with better score
+  score.each_with_index{ |s, i|
+    chosen = i if s >= score[chosen]
   }
   
   if $analyze
